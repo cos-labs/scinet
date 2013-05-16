b12e86122717b947f2991f542e1d92b8b6cd60e3
hrybacki <hrybacki@gmail.com>
2013-05-16 11:15:26 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-crowd-scholar
-=============
+# Test bed for Scholarly crowd sourcing engine
+
+This is a trial version of the Scholarly crowd sourcing engine.
---
+++ app/views.py
@@ -0,0 +1,33 @@
+from flask import render_template, flash, redirect
+from app import app
+from forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = { 'nickname': 'Some Dude' } # placeholder user
+    posts = [ # placeholder posts
+        {
+            'author': {'nickname': 'Timmy'},
+            'body': 'Some article title'
+        },
+        {
+            'author': {'nickname': 'Tom'},
+            'body': 'Another damned article title'
+        }
+    ]
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts) 
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data +'",remember_me=' + str(form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
