2a0ecfa59f8582b9853e217092be6190f6416502
hrybacki <hrybacki@gmail.com>
2013-06-02 01:34:42 -0400

    Takes command line arguments, handles multiple authors, try/catch takes into account necessary vs optional fields. JSON formatting is still ugly.

--- bibtex_to_json.py
+++ bibtex_to_json.py
@@ -1,49 +1,65 @@
+#!/usr/bin/env python
 """
 Test script for converting standard bibtex format to Scholarly json standard
 Written By: Harry Rybacki
-Date: 31May13
-Version: 0.1
+Date: 1June13
+Version: 0.2
+
+usage: ./bibtex_to_json <bibtex file> <output json>
 """
 
+import sys
 import json
 
 from pybtex.database.input import bibtex
 
-# open the bibtex file
+# open bibtex file
 parser = bibtex.Parser()
-data = parser.parse_file(source_file)
+data = parser.parse_file(sys.argv[1])
 
-composite = []
+# container for articles
+articles = []
 
 # iterate through each article
 for id in data.entries:
-    #print data.entries[id].fields
     to_json = {}
     article_fields = data.entries[id].fields
+
+    # grab required fields
     try:
-        to_json.update({"title": article_fields["title"]})
-    except:
-        continue
-    try:
-        to_json.update({"container-title": article_fields["journal"]})
-    except:
-        continue
-    try:
-        to_json.update({"date": article_fields["year"]})
+        to_json.update({unicode("title"): unicode(article_fields["title"])})
+        to_json.update({
+            unicode("date"): unicode(article_fields["year"])})
+        # handle multiple authors
+        authors = []
+        for author in data.entries[id].persons["author"]:
+            authors.append({
+                unicode('given'): unicode(author.first()[0]),
+                unicode('family'): unicode(author.last()[0])
+            })
+        to_json.update({unicode("authors"): authors})
     except:
         continue
 
-    #try:
-    #    to_json.update({"DOI": article_fields["doi"]})
-    #except:
-    #    continue
-    #try:
-    #    to_json.update({"URL": article_fields["url"]})
-    #except:
-    #    continue
+    # grab non-required fields available
+    try:
+        to_json.update({
+            unicode("container-title"): unicode(article_fields["journal"])})
+    except:
+        pass
+    try:
+        to_json.update({"DOI": article_fields["doi"]})
+    except:
+        pass
+    try:
+        to_json.update({"URL": article_fields["url"]})
+    except:
+        pass
 
-    composite.append(json.dumps(to_json))
+    articles.append(json.dumps(to_json))
+    #print json.dumps(to_json)
 
-with open(dest_file, 'w') as output:
-    json.dump(composite, output)
+# write output to json
+with open(sys.argv[2], mode='w') as output:
+    json.dump(articles, output, indent=2)
 
