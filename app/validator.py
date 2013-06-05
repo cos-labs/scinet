3f1028f43d61a47b1799018dfb29dc88ff35d59a
hrybacki <hrybacki@gmail.com>
2013-06-05 11:12:07 -0400

    more validation refactoring

--- app/validator.py
+++ app/validator.py
@@ -1,1 +1,12 @@
-__author__ = 'sphere'
+"""
+Controller for handling api submission data validation requests
+Author: Harry Rybacki
+Date: 5June13
+"""
+
+# @todo implement
+def articles_endpoint_validate(self, data):
+    """ validates data submitted to api article endpoint matches Scholarly
+    JSON standard
+    """
+    raise NotImplementedError
