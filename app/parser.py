2949b1db03d383e65479f8ac18e4203ecc897aa0
hrybacki <hrybacki@gmail.com>
2013-06-05 11:11:32 -0400

    Validation should be directed by its own controller.

--- app/parser.py
+++ app/parser.py
@@ -11,12 +11,6 @@ def article_endpoint_parse(self, data):
     raise NotImplementedError
 
 # @todo implement
-def articles_endpoint_validate(self, data):
-    """ validates data submitted to api article endpoint
-    """
-    raise NotImplementedError
-
-# @todo implement
 def raw_endpoint_parse(self, data):
     """ parses data submitted to the api raw endpoint
     """
