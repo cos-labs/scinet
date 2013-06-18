36e587a238498b0104019f81e384bfe11ebe9859
Joshua Carp <jm.carp@gmail.com>
2013-06-18 09:09:22 -0400

    removed extraction function--no longer needed

--- app/util/html.py
+++ app/util/html.py
@@ -13,29 +13,3 @@ def parse_link(qhtml, link_selector, pattern):
         match = pattern.search(link.get('href', ''))
         if match:
             return match.groups()[0] 
-
-def extract_selectors(pyq, lookups):
-    """ 
-
-    Args:
-        pyq : 
-        lookups : 
-    Returns:
-        ...
-    """
-    # Initialize fields
-    fields = {}
-    
-    # Iterate over field rules
-    for field in lookups:
-     
-        # Extract value from HTML
-        lookup = lookups[field]
-        value = pyq(lookup).text()
-     
-        # Add value if available
-        if value:
-            fields[field] = value
-
-    # Return completed data
-    return fields
