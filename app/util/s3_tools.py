5d99c0d3686740abcb8bce3004d7f321bacba01b
Ubuntu <ubuntu@ip-10-244-154-237.ec2.internal>
2013-07-08 14:50:07 +0000

    Factored out s3 connection and added method for retrieveing keys

--- app/util/s3_tools.py
+++ app/util/s3_tools.py
@@ -3,8 +3,15 @@ import boto
 
 from boto.s3.key import Key 
 
+def _connect():
+    """connects to s3 DB"""
+    try:
+        connection = boto.connect_s3()
+        return connection
+    except NoAuthHandlerFound:
+        print 'Authorization error, check yout /etc/boto.cfg or ~/.boto'
 
-def store_file(bucket, filename, payload):
+def store_file(desired_bucket, filename, payload):
     """adds a file to a specified S3 bucket
         
     :params bucket: destination bucket
@@ -13,16 +20,11 @@ def store_file(bucket, filename, payload):
 
     :returns: boolean of success or failure
     """ 
-
     # instansiate connection with S3
-    try:
-        connection = boto.connect_s3()
-    except NoAuthHandlerFound:
-        print 'Authorization error, check your /etc/boto.cfg or ~/.boto'
-        return False
+    connection = _connect()
 
     # grab the desired bucket
-    bucket = connection.get_bucket(bucket)
+    bucket = connection.get_bucket(desired_bucket)
     if bucket is None:
         print 'Bucket does not exist'
         return False
@@ -37,3 +39,27 @@ def store_file(bucket, filename, payload):
     # return True for acknowledgement
     # @todo: implement try/catches and think of a better return type
     return True
+
+def get_file(bucket, filename):
+    """retrieves and returns an s3 key
+
+    :params bucket: bucket file is in
+    :params filename: key file is stored in
+
+    :return: key instance
+    """
+    connection = boto.connect_s3()
+    
+    # @todo: factor our
+    bucket = connection.get_bucket(bucket)
+    if bucket is None:
+        print 'Bucket does not exist'
+        return False
+
+    # check if key is in bucket
+    for key in bucket.list():
+        if key.key == filename:
+            return key
+    
+    # return None if no key was found
+    return None
